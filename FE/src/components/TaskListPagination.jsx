bd2f96bfda4c99125694bcab5a1a40ddde1aa1f1
Daji-36 <nguyenduongdai31@gmail.com>
2026-03-28 09:36:26 +0700

    Upload: pagination UI

--- FE/src/components/TaskListPagination.jsx
+++ FE/src/components/TaskListPagination.jsx
@@ -1,7 +1,95 @@
-import React from "react";
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+} from "@/components/ui/pagination";
+import { cn } from "../lib/utils";
 
-const TaskListPagination = () => {
-    return <div>TaskListPagination</div>;
+const TaskListPagination = ({
+    handleNext,
+    handlePrev,
+    handlePageChange,
+    page,
+    totalPages,
+}) => {
+    const genaratePages = () => {
+        const pages = [];
+
+        if (totalPages < 4) {
+            // Hiển thị tất cả các trang nếu tổng số trang nhỏ hơn 4
+            for (let i = 1; i <= totalPages; i++) {
+                pages.push(i);
+            }
+        } else if (page <= 2) {
+            // Trang ở đầu
+            pages.push(1, 2, 3, "...", totalPages);
+        } else if (page >= totalPages - 1) {
+            // Trang ở cuối
+            pages.push(1, "...", totalPages - 2, totalPages - 1, totalPages);
+        } else {
+            // Trang ở giữa
+            pages.push(1, "...", page - 1, page, page + 1, "...", totalPages);
+        }
+
+        return pages;
+    };
+
+    const pagesToShow = genaratePages();
+
+    return (
+        <div className="flex justify-center mt-4">
+            <Pagination>
+                <PaginationContent>
+                    {/* Trang trước */}
+                    <PaginationItem>
+                        <PaginationPrevious
+                            onClick={page === 1 ? undefined : handlePrev}
+                            className={cn(
+                                "cursor-pointer",
+                                page === 1 && "pointer-events-none opacity-50",
+                            )} // Vô hiệu hóa và làm mờ nút nếu đang ở trang đầu tiên
+                        />
+                    </PaginationItem>
+
+                    {pagesToShow.map((p, index) => (
+                        <PaginationItem key={index}>
+                            {p === "..." ? (
+                                <PaginationEllipsis />
+                            ) : (
+                                <PaginationLink
+                                    isActive={p === page}
+                                    onClick={() => {
+                                        if (p !== page) handlePageChange(p);
+                                    }}
+                                    className="cursor-pointer"
+                                >
+                                    {p}
+                                </PaginationLink>
+                            )}
+                        </PaginationItem>
+                    ))}
+
+                    {/* Sau */}
+                    <PaginationItem>
+                        <PaginationNext
+                            onClick={
+                                page === totalPages ? undefined : handleNext
+                            }
+                            className={cn(
+                                "cursor-pointer",
+                                page === totalPages &&
+                                    "pointer-events-none opacity-50",
+                            )}
+                        />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
+        </div>
+    );
 };
 
 export default TaskListPagination;
--- FE/src/pages/HomePage.jsx
+++ FE/src/pages/HomePage.jsx
@@ -8,6 +8,7 @@ import DateTimeFilter from "../components/DateTimeFilter";
 import Footer from "../components/Footer";
 import { toast } from "sonner";
 import api from "../lib/axios";
+import { visibleTaskLimit } from "../lib/data";
 
 const HomePage = () => {
     const [taskBuffer, setTaskBuffer] = useState([]); //Buffer: chỗ gom data lại để xử lý tiếp trước khi đưa vào state chính
@@ -18,6 +19,8 @@ const HomePage = () => {
     const [filter, setFilter] = useState("all"); // State để lưu trữ bộ lọc hiện tại (tất cả, đang làm, đã hoàn thành)
 
     const [dateQuery, setDateQuery] = useState("today");
+
+    const [page, setPage] = useState(1);
 
     // gọi API lấy ds công việc
     const fetchTasks = async () => {
@@ -38,6 +41,26 @@ const HomePage = () => {
         fetchTasks(); //chạy 1 lần duy nhất khi component được render
     }, [dateQuery]); // Mỗi khi đổi bộ lọc cần fetch data lại
 
+    useEffect(() => {
+        setPage(1); // Mỗi khi đổi bộ lọc cần reset về trang 1
+    }, [filter, dateQuery]); // Mỗi khi đổi bộ lọc hoặc đổi truy vấn ngày cần reset về trang 1
+
+    const handleNext = () => {
+        if (page < totalPages) {
+            setPage((prev) => prev + 1);
+        }
+    };
+
+    const handlePrev = () => {
+        if (page > 1) {
+            setPage((prev) => prev - 1);
+        }
+    };
+
+    const handlePageChange = (newPage) => {
+        setPage(newPage);
+    };
+
     const handleTaskChanged = () => {
         fetchTasks(); // Gọi lại API để cập nhật danh sách công việc sau khi có sự thay đổi (thêm, sửa, xóa)
     };
@@ -53,6 +76,18 @@ const HomePage = () => {
                 return true; // Hiển thị tất cả công việc nếu không có bộ lọc cụ thể
         }
     });
+
+    // Hàm tách task
+    const visibleTasks = filteredTasks.slice(
+        (page - 1) * visibleTaskLimit,
+        page * visibleTaskLimit,
+    ); // Nếu page = 1 thì lấy từ 0 đến 3, nếu page =2 thì lấy từ 4-7
+
+    if (visibleTasks.length === 0) {
+        handlePrev(); // Nếu không còn task nào để hiển thị trên trang hiện tại sau khi lọc, tự động chuyển về trang trước đó
+    }
+
+    const totalPages = Math.ceil(filteredTasks.length / visibleTaskLimit); // Tổng số trang = tổng số công việc đã lọc / số công việc hiển thị trên mỗi trang
 
     return (
         <div className="min-h-screen w-full relative">
@@ -79,13 +114,19 @@ const HomePage = () => {
                     />
                     {/* Danh sach nhiem vu */}
                     <TaskList
-                        filteredTasks={filteredTasks}
+                        filteredTasks={visibleTasks}
                         filter={filter}
                         handleTaskChanged={handleTaskChanged}
                     />
                     {/* Phan trang va loc theo ngay */}
                     <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
-                        <TaskListPagination />
+                        <TaskListPagination
+                            handleNext={handleNext}
+                            handlePrev={handlePrev}
+                            handlePageChange={handlePageChange}
+                            page={page}
+                            totalPages={totalPages}
+                        />
                         <DateTimeFilter
                             dateQuery={dateQuery}
                             setDateQuery={setDateQuery}
