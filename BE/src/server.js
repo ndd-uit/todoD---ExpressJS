1371c3f19890dafb95e40a5d4938874b7da89c4c
Daji-36 <nguyenduongdai31@gmail.com>
2026-03-28 10:10:03 +0700

    fix(server): correct catch-all route to prevent crash

--- BE/src/server.js
+++ BE/src/server.js
@@ -4,10 +4,10 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
-const PORT = process.env.PORT || 3000;
-const __dirname = path.resolve();
 
 dotenv.config();
+const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -22,7 +22,7 @@ app.use("/api/tasks", tasksRoutes);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../FE/dist"))); // Middleware để phục vụ các file tĩnh từ thư mục build của React
 
-    app.get("*", (req, res) => {
+    app.get(/.*/, (req, res) => {
         res.sendFile(path.join(__dirname, "../FE/dist/index.html")); // Trả về file index.html cho tất cả các route không được định nghĩa ở trên (để React Router có thể xử lý routing phía client)
     });
 }
