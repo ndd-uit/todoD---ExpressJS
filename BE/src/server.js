c648619b6692be2d619ec25a78b76e3150841c06
Daji-36 <nguyenduongdai31@gmail.com>
2026-03-28 09:57:51 +0700

    chore: prepare for deployment

--- BE/src/server.js
+++ BE/src/server.js
@@ -3,19 +3,29 @@ import tasksRoutes from "./routes/tasksRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import path from "path";
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL })); // Middleware để cho phép CORS (Cross-Origin Resource Sharing)
+if (process.env.NODE_ENV === "development") {
+    app.use(cors({ origin: process.env.CLIENT_URL })); // Middleware để cho phép CORS (Cross-Origin Resource Sharing)
+}
 
 app.use(express.json()); // Middleware để parse JSON request body
 
 app.use("/api/tasks", tasksRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../FE/dist"))); // Middleware để phục vụ các file tĩnh từ thư mục build của React
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../FE/dist/index.html")); // Trả về file index.html cho tất cả các route không được định nghĩa ở trên (để React Router có thể xử lý routing phía client)
+    });
+}
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
--- FE/src/lib/axios.js
+++ FE/src/lib/axios.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+const baseURL =
+    import.meta.env.MODE === "development"
+        ? "http://localhost:3000/api"
+        : "/api"; // Đặt base URL tùy thuộc vào môi trường (development hoặc production)
+
 // Tạo 1 instance của axios để có thể cấu hình riêng cho nó nếu cần thiết
 const api = axios.create({
-    baseURL: "http://localhost:3000/api", // Đặt base URL cho tất cả các request
+    baseURL: baseURL, // Đặt base URL cho tất cả các request
 });
 
 export default api;
